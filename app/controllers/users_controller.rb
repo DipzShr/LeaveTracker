1da38c5c26f12d6ef6512f26acb93026d691e496
DipzShr <Dipz.shr@gmail.com>
2015-07-08 16:11:54 +0545

    Add controller users and its index view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  respond_to :json
 	def index
+		
 	end
+
+	def get_events
+    @leaves = current_user.leave_requests
+    events = []
+    @leaves.each do |leave|
+      events << {:id => leave.id, :title => "#{leave.leave_type}", :start => "#{leave.leave_date}",:end => "" }
+    end
+    render :text => events.to_json
+  end
 end
