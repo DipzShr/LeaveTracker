4f073afcca2615658347016e6ceed7e7e045f047
DipzShr <Dipz.shr@gmail.com>
2015-07-08 16:08:49 +0545

    Add controller employees to control employee.

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,40 @@
+class EmployeesController < ApplicationController
+  def index
+    @users = Array.wrap(User.all) - Array.wrap(current_user)
+    respond_to do |format|                                                                                                                                                                                                                                                                                                                                                                          
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    role_name = params[:role]
+    if params[:user_id].present?
+      user = User.find(params[:user_id])
+      user.update_attributes(email: email, name: name)
+      unless user.roles.first.name == role_name
+        role = Role.where(name: role_name).first_or_create
+        user.roles.delete_all
+        binding.pry
+        user.roles << role
+      end
+      flash[:notice] = 'Updated.'
+    else
+      user = User.create(email: email)
+      user.update_attributes(password: 'hetauda04', password_confirmation: 'hetauda04', name: name)
+      user.roles << Role.where(name: role_name).first_or_create
+      flash[:notice] = 'Saved.'
+    end
+    redirect_to employees_path
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.roles.delete_all
+    user.delete
+
+    return redirect_to employees_path
+  end
+end
