8f85aff08fbbf9e5163f54811fb5fa65ad62f379
DipzShr <Dipz.shr@gmail.com>
2015-07-10 12:33:45 +0545

    Get leave requests in appropriate format for calendar.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+
   respond_to :json
 	def index
 		
 	end
 
-	def get_events
-    @leaves = current_user.leave_requests
-    events = []
+  def get_events
+    date = Date.today
+    start_date = date.at_beginning_of_week
+    end_date = date.at_end_of_week
+    @leaves = if current_user.role? :admin
+                LeaveRequest.where('leave_date BETWEEN ? AND ?', start_date, end_date)
+              elsif current_user.role? :employee
+                current_user.leave_requests.where('leave_date BETWEEN ? AND ?', start_date, end_date)
+              end
+    @events = []
     @leaves.each do |leave|
-      events << {:id => leave.id, :title => "#{leave.leave_type}", :start => "#{leave.leave_date}",:end => "" }
+      @events << {:id => leave.id, :title => "#{leave.leave_type.camelize + '-' + leave.status.camelize + ' ' + leave.user.name}", :description => '', 
+      :start => "#{leave.leave_date.to_date}",:end => ""}
     end
-    render :text => events.to_json
+    render :text => @events.to_json
   end
 end
