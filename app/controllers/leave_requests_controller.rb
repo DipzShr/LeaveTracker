709a8e9800f26237ee569bc30a0d7d5ae6abad75
DipzShr <Dipz.shr@gmail.com>
2015-07-10 12:20:13 +0545

    Validate date before creating leave request and send mail on creating leave request.

--- app/controllers/leave_requests_controller.rb
+++ app/controllers/leave_requests_controller.rb
@@ -14,8 +14,41 @@ class LeaveRequestsController < ApplicationController
   	user = User.find(params[:id])
     leave_type = params[:leave_type]
     leave_date = Date.strptime(params[:leave_date], "%m/%d/%Y")
-    leave_request = user.leave_requests.create(leave_type: leave_type, status: 'pending', leave_date: leave_date, duration: 'full_day')
-  	redirect_to leave_requests_path
+    status = current_user.role?('admin') ? 'accepted' : 'pending'
+    if leave_date >= Date.today
+      day = leave_date.strftime('%A')
+      if (day == 'Sunday' || day == 'Saturday')
+        message = "You have selected public holiday: '#{day}'"
+      else
+        if user.leave_requests.map(&:leave_date).map(&:to_date).include?(leave_date)
+          message = "Sorry. Your leave request for the date '#{leave_date}' has already been recorded."
+        else
+          leave_request = user.leave_requests.create(leave_type: leave_type, status: status, leave_date: leave_date, duration: 'full_day')
+          send_mail(leave_request)
+          message = "Your leave request has been successfully recorded for date '#{leave_date}'."
+        end
+      end
+    else
+      message = "You cannot request for the date '#{leave_date}'. The date has already gone."
+    end
+    flash[:notice] = message
+    redirect_to leave_requests_path
+  end
+
+
+  private
+
+  def send_mail(leave)
+    Mail.defaults do
+      delivery_method :smtp, Rails.configuration.action_mailer.smtp_settings
+    end
+    mail = Mail.new do
+      to 'dipz.shr@gmail.com'
+      from 'do-not-reply@hims.com'
+      subject 'Leave Request'
+      body "#{leave.user.name} is requesting leave on date #{leave.leave_date.to_date}."
+    end
+    mail.deliver!
   end
 
 end
