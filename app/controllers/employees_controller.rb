3f56cee9fc6a8f2a6185f1838f596831daa65045
DipzShr <Dipz.shr@gmail.com>
2015-07-11 18:19:12 +0545

    Include cancan authorization for the actions and check valid user while changing password.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,9 @@
 class EmployeesController < ApplicationController
+  authorize_resource :class => :false
+  skip_authorize_resource :only => [:update, :download_pdf, :upload_cv]
 
   def index
-    @users = Array.wrap(User.all) - Array.wrap(current_user)
+    @users = User.where.not(id: current_user.id)
     respond_to do |format|                                                                                                                                                                                                                                                                                                                                                                     
       format.html
       format.js
@@ -13,7 +15,7 @@ class EmployeesController < ApplicationController
     email = params[:email]
     role_name = params[:role]
     if params[:user_id].present?
-      user = User.find(params[:user_id])
+      user = User.find_by_id(params[:user_id])
       user.update_attributes(email: email, name: name)
       unless user.roles.first.name == role_name
         role = Role.where(name: role_name).first_or_create
@@ -26,6 +28,7 @@ class EmployeesController < ApplicationController
       user.roles << Role.where(name: role_name).first_or_create
       flash[:notice] = 'Saved.'
     end
+
     redirect_to employees_path
   end
 
@@ -45,13 +48,19 @@ class EmployeesController < ApplicationController
   end
 
   def update
-    if current_user.valid_password?(params['password0'])
-      current_user.update_attributes(password: params['password1'], password_confirmation: params['password1'])
-      sign_in(current_user, :bypass => true)
-      flash[:success] = 'Password successfully changed.'
+    user = User.find_by_id(params['id'])
+    if current_user.is_valid?(user)
+      if user.valid_password?(params['password0'])
+        user.update_attributes(password: params['password1'], password_confirmation: params['password1'])
+        sign_in(user, :bypass => true)
+        message = 'Password successfully changed.'
+      else
+        message = 'You provided wrong old password.'
+      end
     else
-      flash[:error] = 'You provided wrong old password.'
+      message = 'Invalid Request.'
     end
+    flash[:notice] = message
 
     redirect_to employee_path(current_user.id)
   end
@@ -62,9 +71,9 @@ class EmployeesController < ApplicationController
       if File.extname(datafile.original_filename) == '.pdf'
         user = User.find(params[:employee_id])
         user.upload_cv(params[:upload])
-        flash[:notice] = 'CV successfully updated.'
+        flash[:notice] = 'CV successfully uploaded.'
       else
-        flash[:notice] = 'Wrong extension.'
+        flash[:notice] = 'Wrong file type.'
       end
     else
       flash[:notice] = 'No pdf found.'
@@ -74,6 +83,7 @@ class EmployeesController < ApplicationController
   end
 
   def download_pdf
+    authorize! :download_pdf, :employee
     user = User.find(params[:employee_id])
     send_file("#{Rails.root}/public/CVs/#{user.filename}",
               filename: "#{user.name}.pdf",
