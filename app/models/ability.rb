4dc0c8f5fe97277f13e28552afadfb3c78a88f3d
DipzShr <Dipz.shr@gmail.com>
2015-07-11 18:25:12 +0545

    Correct abilities for employee role.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
       user ||= User.new # guest user (not logged in)
       if user.role? :admin
         can :manage, :all
       elsif user.role? :employee
         can :read, :all
-        can :update, User
         can :get_events, User
-        can :manage, LeaveRequest
+        can :create, LeaveRequest
       end
 
   end
